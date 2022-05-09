c82d9d0b6ccfd0b7cd1154e20d21183be2781fc4
George S. Hugh <hughgs@duke.edu>
2022-05-09 09:36:43 -0400

    Add type annotations.

--- DessertData.py
+++ DessertData.py
@@ -1,14 +1,22 @@
-
-from data.DecisionTreeData import DecisionTreeData
 import numpy as np
 from sklearn.model_selection import train_test_split
+
+from data.DecisionTreeData import DecisionTreeData
+from data.Feature import Feature
 
 
 class DessertData(DecisionTreeData):
     """
-    The dessert data
+    The dessert data. Numerical data is stored as a list of lists. Features is a dictionary using
+    feature as a key and the column as the value.
     """
-    def __init__(self, num_rows, noise=0.0, data=None):
+    def __init__(self, num_rows: int, noise=0.0, data: tuple[list, dict] = None):
+        """
+        Initialize dessert data
+        :param num_rows:
+        :param noise:
+        :param data: tuple of data and features
+        """
         if num_rows < 0:
             raise ValueError("Num of rows >= 0")
 
@@ -19,14 +27,27 @@ class DessertData(DecisionTreeData):
             feature_list = [[0, 1] for _ in range(num_features)]
             mesh = np.array(np.meshgrid(*feature_list))
             self._data = mesh.T.reshape(2 ** num_features, num_features)
-            self._expand(self._num_rows)
+            self._data = self._expand(self._num_rows, self._data)
+            self._features = self._define_features()
 #            self._define_classes()
         else:
-            if num_rows != len(data):
+            self._data, self._features = data
+            if num_rows != len(self._data):
                 raise ValueError("Num of rows must match data")
-            self._data = data
+            if len(self._features) != len(self._data[0]):
+                raise ValueError("Num of features must match data")
 
-    def split(self, percentage):
+    @staticmethod
+    def _define_features():
+        feature_descriptions = ["Eaten", "Hungry", "Healthy", "Tasty"]
+        values = [{"yes": 1, "no": 0}, {"yes": 1, "no": 0}, {"yes": 1, "no": 0}]
+        ret_dict = {}
+        for col_num, (description, value) in enumerate(zip(feature_descriptions, values)):
+            ret_dict[Feature(description, value)] = col_num
+
+        return ret_dict
+
+    def split(self, percentage: float) -> tuple[DecisionTreeData, DecisionTreeData]:
         """
         Split the current data into a training set and a test set. If either set contains
         no data use an empty list.
@@ -35,35 +56,40 @@ class DessertData(DecisionTreeData):
         """
         train_rows = percentage*self._num_rows
         if train_rows < 1:
-            return DessertData(0, 0.0, []), self
+            return DessertData(0, 0.0, ([], {})), self
         test_rows = self._num_rows - train_rows
         if test_rows < 1:
-            return self, DessertData(0, 0.0, [])
+            return self, DessertData(0, 0.0, ([], {}))
         train_data, test_data = train_test_split(self._data, train_size=percentage)
         train_rows = len(train_data)
         test_rows = len(test_data)
         return DessertData(train_rows, self._noise_level, train_data),\
                DessertData(test_rows, self._noise_level, test_data)
 
-    def _expand(self, num_rows):
+    @staticmethod
+    def _expand(num_rows, data):
         """
         Expand/contract data to num_rows by sampling the row numbers. If shrinking, then don't
-        replace the row numbers.
+        replace the row numbers. Make static to enable calling from __init__
         :param num_rows: Final number of rows in data
+        :param data: list of lists of numerical data
         """
-        cur_rows = len(self._data)
+        cur_rows = len(data)
         replace_rows = num_rows > cur_rows
         rows = np.random.choice(cur_rows, size=num_rows, replace=replace_rows)
-        self._data = self._data[rows, :]
+        return data[rows, :]
 
-    def is_single_class(self):
+    def is_single_class(self) -> bool:
         test_value = self._data[0][-1]
         class_data = [row[-1] for row in self._data]
-        return all([elem==test_value for elem in class_data])
+        return all([elem == test_value for elem in class_data])
 
-    def get_max_info_gain(self):
+    def get_max_info_gain(self) -> list:
         """
         Find feature with maximum information gain
         :return: feature
         """
-        return None
+        return list(self._features.keys())[0]
+
+    def remove_feature(self, feature: Feature):
+        pass
